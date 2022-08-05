48cc820d93308b5d5b1b0eef6b22e0476d8a441f
Aleksandar <aleksandardeljanin@gmail.com>
2022-08-05 15:41:58 +0200

    updated tennis-score component

--- src/app/tennis-score/tennis-score.component.ts
+++ src/app/tennis-score/tennis-score.component.ts
@@ -1,28 +1,62 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   // selector: 'app-tennis-score',
   templateUrl: './tennis-score.component.html',
   styleUrls: ['./tennis-score.component.scss'],
 })
 export class TennisScoreComponent {
-  // constructor() {}
-  displayValue1: string = '';
-  displayValue2: string = '';
-  displayCounts: string[] = [];
-  getValue1(value1: string) {
-    console.log(value1);
-    this.displayValue1 = value1;
+  firstPlayerName = '';
+  secondPlayerName = '';
+  firstPlayerScore = '00';
+  secondPlayerScore = '00';
+
+  showMyMessage = false;
+
+
+  addFirstPlayerName(name: string) {
+    this.firstPlayerName = name;
   }
-  getValue2(value2: string) {
-    console.log(value2);
-    this.displayValue2 = value2;
+
+  addSecondPlayerName(name: string) {
+    this.secondPlayerName = name;
   }
-  public showMyMessage = false;
+
+  increaseFirstPlayerResult() {
+    if(this.firstPlayerScore === '00') {
+      this.firstPlayerScore = '15';
+    } else if (this.firstPlayerScore === '15') {
+      this.firstPlayerScore = '30';
+    } else if(this.firstPlayerScore === '30'){
+      this.firstPlayerScore = '40';
+    } else {
+      if(this.secondPlayerScore === '40') {
+        this.firstPlayerScore = 'AD'
+        this.secondPlayerScore = ''
+      }
+    }
+  }
+
+  increaseSecondPlayerResult() {
+    if(this.secondPlayerScore === '00') {
+      this.secondPlayerScore = '15';
+    } else if (this.secondPlayerScore === '15') {
+      this.secondPlayerScore = '30';
+    } else if(this.secondPlayerScore === '30'){
+      this.secondPlayerScore = '40';
+    } else {
+      if(this.secondPlayerScore === '40') {
+        this.secondPlayerScore = 'AD'
+        this.firstPlayerScore = ''
+      }
+    }
+  }  
 
   showMessageSoon() {
     setTimeout(() => {
       this.showMyMessage = true;
     }, 500);
   }
+
 }
